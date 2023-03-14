e59378437ee98c1743e4fa8fa90e136d03b0004a
Max-Ranholi <ranholimax@gmail.com>
2023-03-14 13:14:22 -0300

    conexão com mongoDB

--- dataBase.js
+++ dataBase.js
@@ -6,7 +6,11 @@ main().catch((err) => console.log(err));
 
 async function main() {
   await mongoose.connect(
-    `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.awfudr5.mongodb.net/?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.awfudr5.mongodb.net/?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
   );
   console.log("Banco de dados conectado");
 }
